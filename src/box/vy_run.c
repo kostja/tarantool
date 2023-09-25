553e9a49af984e15626d3f95d1214effe61601de
Vladimir Davydov <vdavydov@tarantool.org>
2023-09-25 10:42:43 +0300

    vinyl: delete run files in single coio call
    Currently, vy_run_remove_files calls coio several times under the hood -
    once per each run file and data directory. Apart from being inefficient,
    this also prevents us from adding some extra logic for thorough file
    deletion. So let's perform all the operations in a single coio call.
    
    Needed for tarantool/tarantool-ee#540
    
    NO_DOC=refactoring
    NO_TEST=refactoring
    NO_CHANGELOG=refactoring

--- src/box/vy_run.c
+++ src/box/vy_run.c
@@ -2507,8 +2507,13 @@ vy_run_remove_files_f(va_list ap)
 	for (int type = 0; type < vy_file_MAX; type++) {
 		vy_run_snprint_path(path, sizeof(path), dir,
 				    space_id, iid, run_id, type);
-		if (!xlog_remove_file(path, XLOG_RM_VERBOSE))
-			ret = -1;
+		if (unlink(path) < 0) {
+			if (errno != ENOENT) {
+				say_syserror("error while removing %s", path);
+				ret = -1;
+			}
+		} else
+			say_info("removed %s", path);
 	}
 	/* Remove the root directory if it's empty. */
 	vy_lsm_snprint_path(path, sizeof(path), dir, space_id, iid);
